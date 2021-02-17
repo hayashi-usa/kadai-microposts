053292b24ccedc52de800b8ae882d606dee6a350
hayashi <lestecha11@gmail.com>
2021-02-17 09:35:29 +0000

    post

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,6 @@ Rails.application.routes.draw do
   
   get 'signup', to: 'users#new'
   resources :users, only: [:index, :show, :create, :new]
+  
+  resources :microposts, only: [:create, :destroy]
 end
